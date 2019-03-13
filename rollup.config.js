2b2341ab3053d1cc8c029e8003264805489c7fa9
Max Inno <maksym.hubach@gmail.com>
2019-03-13 16:52:50 +0200

    configs update & refactoring

--- rollup.config.js
+++ rollup.config.js
@@ -2,27 +2,26 @@
 import pkg from "./package.json";
 import minify from "rollup-plugin-minify-es"
 import resolve from "rollup-plugin-node-resolve"
-import sourcemaps from 'rollup-plugin-sourcemaps'
+import sourcemaps from "rollup-plugin-sourcemaps"
 import typescript from "rollup-plugin-typescript2"
 
-export default [
-  {
-    input: "src/core.ts",
-    external: [
-      "querystring",
-      ...Object.keys(pkg.dependencies)
-    ],
-    plugins: [
-      minify(),
-      resolve(),
-      sourcemaps(),
-      typescript()
-    ],
-    output: {
-      file: `${pkg.main}.js`,
-      format: "cjs",
-      exports: "named",
-      sourcemap: true
-    }
+export default {
+  input: "src/core.ts",
+  external: [
+    "path", "querystring",
+    ...Object.keys(pkg.dependencies),
+    "lowdb/adapters/FileSync"
+  ],
+  plugins: [
+    minify(),
+    resolve(),
+    sourcemaps(),
+    typescript()
+  ],
+  output: {
+    file: `${pkg.main}.js`,
+    format: "cjs",
+    exports: "named",
+    sourcemap: true
   }
-]
+}
